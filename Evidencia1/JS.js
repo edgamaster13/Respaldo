0cb29cf0f702b91ce7ecb855cdd8fbb947bd8a96
edgamaster13 <47649148+edgamaster13@users.noreply.github.com>
2019-02-22 12:28:04 -0600

    Add files via upload

---
+++ Evidencia1/JS.js
@@ -0,0 +1,12 @@
+function gusto() {
+    alert("Que bueno que te alla gustado");
+}
+function NoLeGusto() {
+    alert("Me vale");
+}
+function Hora(){
+    document.getElementById('lahora').innerHTML = Date();
+}
+function Recargar(){
+    location.reload();
+}
